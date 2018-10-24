04757c99af704a83ebbc8135e9115c07f6574cbd
think <310445479@qq.com>
2018-10-24 15:51:27 -0400

    add elementary sort

---
+++ 03-ElementarySorts/Selection.java
@@ -0,0 +1,36 @@
+package ElementarySorts;
+
+public class Selection {
+	private static boolean less(Comparable v, Comparable w){
+		return v.compareTo(w) < 0;
+	}
+	
+	private static void exch(Comparable[] a, int i, int j){
+		Comparable swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
+	
+	private static boolean isSorted(Comparable[] a){
+		for(int i=1; i<a.length; i++){
+			if(less(a[i], a[i-1])){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void sort(Comparable[] a){
+		int N = a.length;
+		for(int i=0; i<N; i++){
+			int min=i;
+			for(int j=0; j<N; j++){
+				if(less(a[j], a[min])){
+					min = j;
+				}
+			}
+			exch(a, i, min);
+		}
+	}
+
+}
